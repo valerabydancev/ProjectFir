54fd5774e5057346160d39d5ccc7baec145cda9b
valerabydancev <valera.bydancev@mail.ru>
2016-09-21 15:03:28 +0500

    15:03

--- test/test/holsted.xaml.cs
+++ test/test/holsted.xaml.cs
@@ -23,38 +23,46 @@ namespace test
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            int n1 = Convert.ToInt32(tex1.Text);
-            int n2 = Convert.ToInt32(tex2.Text);
-            int N1 = Convert.ToInt32(TEX1.Text);
-            int N2 = Convert.ToInt32(TEX2.Text);
-
-            int N = n1 + n2; //словарь программы
-            int NLength = N1 + N2; //длина реализации
-            double PLength = Math.Round((n1 * Math.Log(n1, 2)) + (n2 * Math.Log(n2, 2)), 2); //Длина программы
-            double V = Math.Round((N1 + N2) * Math.Log(n1 + n2, 2), 2); //Объем программы в битах
-            double V1 = Math.Round((n2 + 2) * Math.Log(n2 + 2, 2), 2); //Потенциальный объем программы
-            double L =  Math.Round((V1 / V), 2); //Уровень программы
-            double I =  Math.Round(L * V, 2); //Интелектуальное содержание программы
-            double E =  Math.Round(V / L, 2); //Работа по программированию
-
-            Label1.Content = "Словарь программы = " + N;
-            Label2.Content = "Длина реализации = " + NLength;
-            Label3.Content = "Длина программы = " + PLength;
-            Label4.Content = "Объем программы в битах = " + V;
-            Label5.Content = "Потенциальный объем программы = " + V1;
-            Label6.Content = "Уровень программы = " + L;
-            Label7.Content = "Интелект. содержание программы = " + I;
-            Label8.Content = "Работа по программированию = " + E;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void back_hol_Click(object sender, RoutedEventArgs e)
         {
             MainWindow MW = new MainWindow();
             this.Hide();
             MW.Show();
             this.Close();
+        }
+
+        private void Button_Click123(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n1 = Convert.ToInt32(tex1.Text);
+                int n2 = Convert.ToInt32(tex2.Text);
+                int N1 = Convert.ToInt32(TEX1.Text);
+                int N2 = Convert.ToInt32(TEX2.Text);
+
+                int N = n1 + n2; //словарь программы
+                int NLength = N1 + N2; //длина реализации
+                double PLength = Math.Round((n1 * Math.Log(n1, 2)) + (n2 * Math.Log(n2, 2)), 2); //Длина программы
+                double V = Math.Round((N1 + N2) * Math.Log(n1 + n2, 2), 2); //Объем программы в битах
+                double V1 = Math.Round((n2 + 2) * Math.Log(n2 + 2, 2), 2); //Потенциальный объем программы
+                double L = Math.Round((V1 / V), 2); //Уровень программы
+                double I = Math.Round(L * V, 2); //Интелектуальное содержание программы
+                double E = Math.Round(V / L, 2); //Работа по программированию
+
+                Label1.Content = "Словарь программы = " + N;
+                Label2.Content = "Длина реализации = " + NLength;
+                Label3.Content = "Длина программы = " + PLength;
+                Label4.Content = "Объем программы в битах = " + V;
+                Label5.Content = "Потенциальный объем программы = " + V1;
+                Label6.Content = "Уровень программы = " + L;
+                Label7.Content = "Интелект. содержание программы = " + I;
+                Label8.Content = "Работа по программированию = " + E;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Заполните поля корректно");
+            }
+            
         }
     }
 
